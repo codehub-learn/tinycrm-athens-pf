54c50cc57c057ff110a2b30a1e4b1e8003193604
dpnevmatikos <pnevmatikos@gmail.com>
2020-04-30 20:01:41 +0300

    Implement Products file parsing

---
+++ TinyCrm/TinyCrm/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TinyCrm
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public DateTime Created { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string VatNumber { get; private set; }
+        public string Phone { get; set; }
+        public decimal TotalGross { get; private set; }
+        public bool IsActive { get; set; }
+        public int Age { get; set; }
+
+        public Customer(string vatNumber)
+        {
+            if (!IsValidVatNumber(vatNumber)) {
+                throw new Exception("Invalid VatNumber");
+            }
+
+            VatNumber = vatNumber;
+            Created = DateTime.Now;
+        }
+
+        public bool IsValidVatNumber(string vatNumber)
+        {
+            return
+                !string.IsNullOrWhiteSpace(vatNumber) &&
+                vatNumber.Length == 9;
+        }
+
+        public bool IsAdult()
+        {
+            return Age >= 18;
+        }
+    }
+}
--- TinyCrm/TinyCrm/Program.cs
+++ TinyCrm/TinyCrm/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TinyCrm
 {
@@ -6,8 +7,57 @@ namespace TinyCrm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World 2!");
+            string[] productsFromFile;
+
+            try {
+                productsFromFile = File.ReadAllLines("products.csv");
+            } catch (Exception) {
+                return;
+            }
+
+            if (productsFromFile.Length == 0) {
+                return;
+            }
+
+            var productsArray = new Product[productsFromFile.Length];
+
+            for (var i = 0; i < productsFromFile.Length; i++) {
+                var isDuplicate = false;
+                var values = productsFromFile[i].Split(';');
+
+                foreach (var p in productsArray) {
+                    if (p != null && p.ProductId.Equals(values[0])) {
+                        isDuplicate = true;
+                    }
+                }
+
+                if (!isDuplicate) {
+                    var product = new Product()
+                    {
+                        ProductId = values[0],
+                        Name = values[1],
+                        Description = values[2],
+                        Price = GetRandomPrice()
+                    };
+
+                    productsArray[i] = product;
+                }
+            }
+
+            foreach(var p in productsArray) {
+                if (p != null) {
+                    Console.WriteLine($"{p.ProductId} {p.Name} {p.Price}");
+                }
+            }
+        }
+
+        public static decimal GetRandomPrice()
+        {
+            var random = new Random();
+            var randomNumber = random.NextDouble() * 100;
+            var roundedNumber = Math.Round(randomNumber, 2);
+
+            return (decimal)roundedNumber;
         }
     }
 }
