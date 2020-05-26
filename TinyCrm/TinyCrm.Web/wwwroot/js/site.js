b0d34afc4e67b6d41fea544a2869d7ecc31da02f
dpnevmatikos <pnevmatikos@gmail.com>
2020-05-26 20:10:40 +0300

    Introduce DOM manipulation & AJAX calls

--- TinyCrm/TinyCrm.Web/wwwroot/js/site.js
+++ TinyCrm/TinyCrm.Web/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let successAlert = $('.js-success-alert');
+successAlert.hide();
+
+let failedAlert = $('.js-fail-alert');
+failedAlert.hide();
+
+let button = $('.js-button');
+
+button.on('click', () => {
+    successAlert.hide();
+    failedAlert.hide();
+
+    let firstname = $('.js-firstname');
+    let lastname = $('.js-lastname');
+    let vatnumber = $('.js-vatnumber');
+
+    let data = {
+        firstName: firstname.val(),
+        lastname: lastname.val(),
+        vatnumber: vatnumber.val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '/customer',
+        contentType: 'application/json',
+        data: JSON.stringify(data)
+    }).done(customer => {
+        successAlert.html(`Customer ${customer.firstname} ${customer.lastname} with id ${customer.customerId} was created`);
+        successAlert.show();
+    }).fail(failureResponse => {
+        failedAlert.show();
+    });
+});
+
